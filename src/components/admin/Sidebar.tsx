94c59e360e7399cfcdaa3ff72275e0d915983c12
Orathai Saengsoemsap <mayomsaki@gmail.com>
2025-09-17 20:45:32 +0700

    feat: add admin layout and sidebar components with navigation links and create file path for each page.

---
+++ src/components/admin/Sidebar.tsx
@@ -0,0 +1,85 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+const menuItems = [
+  { 
+    label: "Customer Booking", 
+    href: "/admin/bookings",
+    icon: "/assets/booking-green.png",
+  },
+  { 
+    label: "Room Management", 
+    href: "/admin/rooms",
+    icon: "/assets/manage-green.png",
+  },
+  { 
+    label: "Hotel Information", 
+    href: "/admin/hotel-info",
+    icon: "/assets/hotel-green.png",
+  },
+  { 
+    label: "Room & Property", 
+    href: "/admin/room-types",
+    icon: "/assets/room-green.png",
+  },
+  { 
+    label: "Analytics Dashboard", 
+    href: "/admin/analytics",
+    icon: "/assets/analytic-green.png",
+  },
+  { 
+    label: "Chatbot Setup", 
+    href: "/admin/chatbot",
+    icon: "/assets/chat-green.png", 
+  },
+];
+
+export default function Sidebar() {
+  const router = useRouter();
+
+  return (
+    <aside className="w-68 min-h-screen bg-green-800 flex flex-col">
+  {/* Logo */}
+  <div className="flex items-center justify-center mt-10">
+    <img className="w-35" src="/assets/logo-white.png" alt="logo" />
+  </div>
+  <div className="flex justify-center items-center font-inter font-light text-xl mt-6 text-green-300">
+    Admin Panel Control
+  </div>
+
+  {/* Navigation */}
+  <nav className="mt-25 flex-1 w-full">
+    {menuItems.map((item) => (
+      <div key={item.href}>
+        <Link
+          href={item.href}
+          className={`flex items-center w-full px-7 py-5 mb-2 text-md font-medium text-green-300 hover:bg-green-600 ${
+            router.pathname === item.href ? "bg-green-600 text-green-300" : ""
+          }`}
+        >
+          <img className="w-6 h-6 mr-3" src={item.icon} alt={item.label} />
+          <span>{item.label}</span>
+        </Link>
+      </div>
+    ))}
+  </nav>
+  {/* Logout Button at the bottom */}
+  <div className="mt-auto mb-30 border-t-1 border-green-100/20">
+    <button
+      onClick={() => {
+        // Handle logout logic here
+        console.log("Logout clicked");
+      }}
+      className="flex items-center w-full px-7 py-5 text-md font-inter font-medium text-green-300 hover:bg-green-600 hover:text-white transition-colors"
+    >
+      <img
+        className="w-6 h-6 mr-4"
+        src="/assets/logout.png"
+        alt="Logout"
+      />
+      Log Out
+    </button>
+  </div>
+</aside>
+  );
+}
---
+++ src/pages/admin/room-types/index.tsx
@@ -0,0 +1,9 @@
+import Layout from "@/components/admin/Layout"
+
+export default function index() {
+  return (
+    <Layout>
+      Room & Property 
+    </Layout>
+  )
+}
