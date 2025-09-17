35593782c37aa5a8386a37e997c4311e1919ba7e
Thanisorn <thanisorn.sir@gmail.com>
2025-09-17 12:48:15 +0700

    chore: scaffold customer login/logout pages and API routes

---
+++ src/pages/customer/logout.tsx
@@ -0,0 +1,8 @@
+export default function CustomerLogoutPage() {
+    return (
+      <main>
+        <h1>Customer Logout Page</h1>
+        {/* ปุ่ม logout จะมาเติมทีหลัง */}
+      </main>
+    );
+  }  
