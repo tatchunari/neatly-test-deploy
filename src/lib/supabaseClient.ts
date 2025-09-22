29b4141425f9c5fc902cce64a544ba478635ff79
ningnong239 <ningnong2539@gmail.com>
2025-09-22 12:24:13 +0700

    feat: add SearchBox component for booking

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,10 +1,18 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-// export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+if (!supabaseUrl) {
+  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_URL')
+}
+if (!supabaseAnonKey) {
+  throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY')
+}
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-    auth: { persistSession: true, autoRefreshToken: true },
+    auth: { 
+      persistSession: true, // เก็บ session ค้างไว้ใน localStorage
+      autoRefreshToken: true // ต่ออายุ token ให้อัตโนมัติ
+    },
   });
---
+++ src/pages/customer/search-result/index.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar"
+
+function index() {
+  return (
+    <div>
+      <Navbar />
+    
+    </div>
+  )
+}
+
+export default index
