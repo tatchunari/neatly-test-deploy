638d88ca8be30dc1ec0708f9da809ca91c55a5d6
Thanisorn <thanisorn.sir@gmail.com>
2025-09-22 11:56:54 +0700

    feat: create CustomerLoginPage component with integrated LoginForm and responsive layout

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -3,8 +3,9 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string
 
-// export const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
-    auth: { persistSession: true, autoRefreshToken: true },
+    auth: { 
+      persistSession: true, // เก็บ session ค้างไว้ใน localStorage
+      autoRefreshToken: true // ต่ออายุ token ให้อัตโนมัติ
+    },
   });
