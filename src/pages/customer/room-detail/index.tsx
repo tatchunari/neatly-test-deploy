5c042af0585549c86f5791056ddb793a16910659
ningnong239 <ningnong2539@gmail.com>
2025-09-23 13:45:20 +0700

    feat: Navbar and SearchBox component

---
+++ src/pages/customer/room-detail/index.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/Navbar"
+
+import { useEffect, useState } from "react"
+
+function Roomdetailpage() {
+  return (
+    <div>
+      <Navbar />
+     
+      
+    </div>
+  )
+}
+
+export default Roomdetailpage
