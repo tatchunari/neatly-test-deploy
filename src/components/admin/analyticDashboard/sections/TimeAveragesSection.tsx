5b429ba666d1a5e5ad75d10dd6f683fe557701b3
Orathai Saengsoemsap <mayomsaki@gmail.com>
2025-10-07 17:33:24 +0700

    feat: enhance responsiveness for admin analysis dashboard page

--- src/components/admin/analyticDashboard/sections/TimeAveragesSection.tsx
+++ src/components/admin/analyticDashboard/sections/TimeAveragesSection.tsx
@@ -92,12 +92,12 @@ const TimeAveragesSection: React.FC<TimeAveragesSectionProps> = ({
   return (
     <div className="flex flex-col bg-white rounded-lg shadow-md sm:p-8 w-full mt-10">
       <div className="flex md:flex-row flex-col w-full justify-between">
-        <h2 className="mt-6 sm:mt-0 ml-3 sm:ml-0 text-lg font-semibold text-gray-700">
+        <h2 className="mt-6 sm:mt-0 ml-5 md:text-lg text-xl font-semibold text-gray-700">
           Check-in and Check-out Times Averages
         </h2>
       </div>
       {/* Check-in & Check-out Card Section */}
-      <div className="grid grid-cols-2 gap-4 mt-5">
+      <div className="grid md:grid-cols-2 gap-4 mt-5 md:p-0 p-5">
         <AverageTimeCard
           icon={Building}
           title="Check-in"
