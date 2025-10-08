44cada7348bcdc345d73e11e601d05926463de9d
Pon Thanat <aaponaa2632@gmail.com>
2025-10-08 20:13:42 +0700

    chore: accept current across repo and clear all conflict markers

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -3,7 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
-<<<<<<< HEAD
 if (!supabaseUrl) {
   throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_URL')
 }
@@ -11,8 +10,6 @@ if (!supabaseAnonKey) {
   throw new Error('Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY')
 }
 
-=======
->>>>>>> dd737d9 (feat: create CustomerLoginPage component with integrated LoginForm and responsive layout)
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     auth: { persistSession: true, autoRefreshToken: true },
     realtime: {
@@ -20,12 +17,4 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
         eventsPerSecond: 10,
       },
     },
-<<<<<<< HEAD
-    realtime: {
-      params: {
-        eventsPerSecond: 10,
-      },
-    },
-=======
->>>>>>> dd737d9 (feat: create CustomerLoginPage component with integrated LoginForm and responsive layout)
   });
